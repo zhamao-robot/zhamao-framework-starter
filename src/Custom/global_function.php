aa236160fd0638fe6d5d867c00d302d52b171a46
jerry <jesse2061@outlook.com>
2021-03-25 17:03:20 +0800

    update to 2.4 version

--- src/Custom/global_function.php
+++
@@ -1,6 +0,0 @@
-<?php /** @noinspection PhpFullyQualifiedNameUsageInspection */ #plain
-
-//这里写你的全局函数
-function pgo(callable $func, $name = "default") {
-    \ZM\Utils\CoroutinePool::go($func, $name);
-}
